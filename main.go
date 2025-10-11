5166c1ec39a4c3477e4a3fe6b2b489dacb4f4160
Malene-Kavanagh <makavanagh@csuchico.edu>
2025-10-11 12:50:35 -0700

    got the timestamp to the correct format

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 )
 
 type Intro struct {
-	Message string    `json:"message"`
-	Time    time.Time `json:"timestamp"`
+	Message string `json:"message"`
+	Time    int64  `json:"timestamp"`
 }
 
 // func Convert(t time.Time) string {
@@ -22,19 +22,13 @@ func main() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		//current time
 		//t := time.Now()
-		t := time.Now().UTC()
-		// t := Convert(time.Now())
+		t := time.Now().UTC().Unix()
 
-		// func Convert(t time.Time) UTC
-		//format time to JSON
-		//timeStamp := t.MarshalJSON()
 		intro := Intro{
 			Message: "My name is Malene Kavanagh",
 			Time:    t,
 		}
 
-		fmt.Println(intro)
-		// test output to show the info being sent
 		fine, err := json.Marshal(intro)
 		if err != nil {
 			panic(err)
@@ -44,7 +38,8 @@ func main() {
 		c.Set(fiber.HeaderContentType, fiber.MIMEApplicationJSON)
 		//c.SendString(intro)
 		//return c.JSON(fine)
-		return c.JSON(intro)
+		return c.Send(fine)
+		//^  this gives garbled output
 		//^accidentally made json.Marshal useless
 	})
 
