e4698a13bdc1d51f3aec793cd8d2399834b3c86e
Malene-Kavanagh <makavanagh@csuchico.edu>
2025-10-15 18:57:03 -0700

    can handle lots of hosts, not hard coded

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -18,7 +22,12 @@ type Intro struct {
 // }
 
 func main() {
-	app := fiber.New()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+	}
+	//app := fiber.New()  // default config
+	app := fiber.New(fiber.Config{})
 	app.Get("/", func(c *fiber.Ctx) error {
 		//current time
 		//t := time.Now()
@@ -43,6 +52,37 @@ func main() {
 		//^accidentally made json.Marshal useless
 	})
 
-	app.Listen(":80")
-	//app.Listen(":8080") // for cloud run
+	app.Get("/port", func(c *fiber.Ctx) error {
+		if p := c.Get("Forwarded-Port"); p != "" {
+			return c.SendString(p)
+		}
+		if p := extractPort(c.Get("Forwarded-Host")); p != "" {
+			return c.SendString(p)
+		}
+		return c.SendString(extractPort(string(c.Context().Request.Header.Peek("Host"))))
+	})
+	log.Fatal(app.Listen(":" + port))
+	//port is hardcoded to be 80
+	//log.Fatal(app.Listen(":80"))
+	//app.Listen(":80")
+}
+
+func extractPort(hostport string) string {
+	//takes a string of host:port and returns port
+	if hostport == "" || !strings.Contains(hostport, ":") {
+		return ""
+	} // no port empty string return
+	if _, p, err := net.SplitHostPort(hostport); err == nil && p != "" {
+		return p
+	} //splits host and port
+	// Some proxies may send multiple hosts, take the first
+	if idx := strings.Index(hostport, ","); idx > 0 {
+		hostport = strings.TrimSpace(hostport[:idx])
+		// underscore ignores error
+		if _, p, err := net.SplitHostPort(hostport); err == nil && p != "" {
+			return p
+		}
+	}
+	return ""
+	//if none worked return empty string
 }
